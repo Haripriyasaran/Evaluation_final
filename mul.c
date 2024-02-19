5b225689bda6990823292df4d8eb61c2af793280
Lalitha N <lalitha19n@gmail.com>
2024-02-19 11:58:08 +0530

    changes

--- mul.c
+++ mul.c
@@ -13,7 +13,7 @@ int main() {
     
     add = num1 + num2;
     
-    printf("The sum of %d and %d is %d.", num1, num2, result);
+    printf("The sum of %d and %d is %d.", num1
     
     return 0;
 }
