53ab76e77f064406145138ca5bf2dde09f935723
unknown <sreeakshay10@gmail.com>
2024-02-19 12:40:00 +0530

    first commit

--- add.c
+++ add.c
@@ -1,10 +1,4 @@
-#include <stdio.h>
-
-int main() {
-    int num1, num2, sum;
-    
-    printf("Enter the first number: ");
-    scanf("%d", &num1);
+#
     
     printf("Enter the second number: ")
     scanf("%d", &num2);
