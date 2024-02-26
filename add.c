b39ff28b45cdbf32cde8f1de21212668283621c4
Nayanagowda0 <nayanagowda053@gmail.com>
2024-02-26 12:45:41 +0530

    first commit

--- add.c
+++ add.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 void main()
  {    
-
-    void number2, number4, sum;
-    
-    printf("Enter two integers: ");
+ int number2, number4, sum;
+       printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
     sum = number2 + number4;      
     printf("%d + %d = %d", number2, number4, sum);
