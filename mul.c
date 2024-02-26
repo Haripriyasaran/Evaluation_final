b6a575709d5be297f85cc961d7cc2adae0f440ba
Shaan Ashraf <157344264+Shaanashraf0077@users.noreply.github.com>
2024-02-26 12:37:28 +0530

    Update mul.c

--- mul.c
+++ mul.c
@@ -3,11 +3,8 @@ void main()
 {    
 
     int number1, number2, sum;
-    
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
-
     sum = number1 + number2;      
-    
     printf("%d + %d = %d", number1, number2, sum)
 }
