225ba2f0fafb914a6060d790ee116465930eb00d
Hema <155943450+22bcad27Hema@users.noreply.github.com>
2024-02-26 12:22:22 +0530

    Update div.c

--- div.c
+++ div.c
@@ -6,11 +6,5 @@ int main()
       printf("\tEnter Two Numbers\n");
       printf("---------------------------\n");
       printf("Enter First Number  : ");
-      scanf("%d", &num1);
-      printf("\nEnter Second Number : ");
-      scanf("%d",&num2);
-      /*div defined as a float & cast any one variable num1 or num2.*/
-      div=num1/(float)num2; //num2 cast as float
-      printf("\nDivision of %d & %d is = %f",num1,num2,div);
-      return 0;
+     
 }
