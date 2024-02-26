31fa16c0eeb5424fd18bcbc8eeb77f44c327f4ad
Shaanashraf0077 <22bcad58@kristujayanti.com>
2024-02-26 12:42:16 +0530

    commit

--- mul.c
+++ mul.c
@@ -5,6 +5,7 @@ void main()
     int number1, number2, sum;
     printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
-    sum = number1 + number2;      
+    sum = number1 + number2;
+          
     printf("%d + %d = %d", number1, number2, sum)
 }
