f7a951edcff817de46a844bbba363ab3f73e6855
Nayanagowda <nayanagowda053@gmail.com>
2024-03-11 11:47:28 +0530

    Update add.c
    commit for merge conflict

--- add.c
+++ add.c
@@ -1,9 +1,9 @@
 #include <stdio.h>
 void main()
  {    
- int number2, number4, sum;
+ int number1, number2, sum;
        printf("Enter two integers: ");
     scanf("%d %d", &number1, &number2);
-    sum = number2 + number4;      
+    sum = number1 + number2;      
     printf("%d + %d = %d", number2, number4, sum);
 }
