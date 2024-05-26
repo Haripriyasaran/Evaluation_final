e9808b3550c3a07f2d043bbd379c7720c45df6b6
Akshay <117372092+Ak-2003-ai@users.noreply.github.com>
2024-05-26 12:11:19 +0530

    Update mul.c

--- mul.c
+++ mul.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 int main() {
   int n;
-  printf("Enter an integer: ");
-  scanf("%d", &n);
+  printf("Enter an inte
 
   for (int i = 1; i <= 
     printf("%d * %d = %d \n", n, i, n * i);
